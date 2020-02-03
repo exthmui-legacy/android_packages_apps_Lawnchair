8750b5c1bd9fd7ad4bd9fe8efefbb1204faf8f84
cjybyjk <cjybyjk@gmail.com>
2020-02-03 17:28:54 +0800

    inital commit

---
+++ Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Lawnchair
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := Lawnchair.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_REQUIRED_MODULES := \
+	lawnchair.permissions \
+	lawnchair.hiddenapi_whitelist
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lawnchair.permissions
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := permissions/privapp-permissions-lawnchair.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lawnchair.hiddenapi_whitelist
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := permissions/lawnchair-hiddenapi-package-whitelist.xml
+include $(BUILD_PREBUILT)
